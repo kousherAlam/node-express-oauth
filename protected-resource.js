7ad69b8b7cdddc03287a6b50f5e9aa89e7302b93
kousher alam <kousheralampranto@gmail.com>
2021-02-06 18:15:36 +0600

    completed scope implemantation.

--- protected-resource.js
+++ protected-resource.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const fs = require("fs")
-const { timeout } = require("./utils")
+const { timeout } = require("./utils");
+const jwt = require("jsonwebtoken");
+const { verify } = require('sinon');
+const { response } = require('express');
 
 const config = {
 	port: 9002,
@@ -31,6 +34,27 @@ app.use(bodyParser.urlencoded({ extended: true }))
 /*
 Your code here
 */
+
+app.get('/user-info', (req, res) =>{
+	const authorization = req.headers.authorization;
+	if(!authorization){
+		return res.status(401).send("Not found any token to varify.");
+	}
+	const token = authorization.slice(7);
+	try{
+		const info =  jwt.verify(token, config.publicKey, {algorithms: ['RS256']}, );
+		const userData = users[info.userName];
+		const scope = info.scope.split(" ");
+		const backData = {};
+		for(let i =0; i<scope.length; i++){
+			const singleValue = scope[i].slice(11);
+			backData[singleValue] = userData[singleValue];
+		}
+		return res.status(200).json(backData);
+	} catch(err){
+		return res.status(401).send(err);
+	}
+});
 
 const server = app.listen(config.port, "localhost", function () {
 	var host = server.address().address
